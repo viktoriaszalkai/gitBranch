300a2391db7a1b4e63faff4b0d933d5987554457
szalkai.v.csenge <szalkai.v.csenge@host.docker.internal>
2024-09-30 13:27:23 +0200

    olvashatobb kod

--- src/gitbranch/GitBranch.java
+++ src/gitbranch/GitBranch.java
@@ -6,11 +6,12 @@ public class GitBranch {
 
   
     public static void main(String[] args) {
-        int i = fv();
+        int i = GitBranch.fv();
     }
 
     private static int fv() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+    
     
 }
