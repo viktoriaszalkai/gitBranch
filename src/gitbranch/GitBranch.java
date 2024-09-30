7649ddbb52619cd2ee7c5ed679defa04bad96748
szalkai.v.csenge <szalkai.v.csenge@host.docker.internal>
2024-09-30 13:23:21 +0200

    nem static int fv

--- src/gitbranch/GitBranch.java
+++ src/gitbranch/GitBranch.java
@@ -6,11 +6,13 @@ public class GitBranch {
 
   
     public static void main(String[] args) {
-        int i = fv();
+        GitBranch obj = new GitBranch();
+        int i = obj.fv();
     }
 
-    private static int fv() {
+    private int fv() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+    // létrehozunk egy másik ágat és azon fogunk vátoztatásokat létrehozni
     
 }
